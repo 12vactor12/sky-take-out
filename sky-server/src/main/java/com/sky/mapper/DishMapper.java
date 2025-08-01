e2c2d90643fb4cf227096dacca64c9e54b989500
12vactor12 <1212vactor@gmail.com>
2025-08-01 13:28:26 +0800

    添加菜品
    实现添加菜品（包含图片上传和菜品口味添加）

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,17 +1,27 @@
 package com.sky.mapper;
 
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-
+import com.sky.annotation.AutoFill;
 @Mapper
 public interface DishMapper {
 
     /**
-     * 根据分类id查询菜品数量
+     * 根据分类id查询菜品数量(用于查看分类是否有关联的菜品)
      * @param categoryId
      * @return
      */
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+
+    /**
+     * 插入菜品数据
+     *
+     * @param dish
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
