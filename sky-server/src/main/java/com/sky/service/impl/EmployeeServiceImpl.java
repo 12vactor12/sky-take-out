9cde7e8780fdc496e342a3ac7d2099af109c9601
12vactor12 <1212vactor@gmail.com>
2025-08-01 09:34:13 +0800

    AOP公共字段填充
    使用自定义注解和AOP类填充create_time, update_time, create_user, update_user
    而不是在每个业务层重复填充

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -79,15 +79,6 @@ public class EmployeeServiceImpl implements EmployeeService {
         //设置密码，默认密码123456(使用md5加密)
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
-        //设置当前记录的创建时间和修改时间
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
-
-        //T设置当前记录创建人id和修改人id
-        Long empId = BaseContext.getCurrentId();
-        employee.setCreateUser(empId);
-        employee.setUpdateUser(empId);
-
         employeeMapper.insert(employee);
     }
 
@@ -129,10 +120,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //对象属性拷贝
         BeanUtils.copyProperties(employeeDTO, employee);
-        //修改时间
-        employee.setUpdateTime(LocalDateTime.now());
-        //修改人id
-        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.update(employee);
     }
